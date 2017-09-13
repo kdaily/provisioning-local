36ba797c94479ed41d1059e494d39c5589932426
kdaily <kdaily@users.noreply.github.com>
2017-09-12 23:29:58 -0700

    add more tasks

--- run.sh
+++ run.sh
@@ -12,5 +12,10 @@ cd provisioning-local
 
 echo -e "please make sure to edit the config.json file followed by [ENTER]" && read USELESS_VAR
 
-ansible-playbook setup.yml -i HOSTS --ask-sudo-pass --module-path ./ansible_modules --extra-vars "@config.json"
-
+if [ ! -f /home/kdaily/.ssh/id_rsa.pub ]
+then
+  echo "No public SSH key found - be sure it is added to Github as well."
+  exit 1
+else
+  ansible-playbook setup.yml -i HOSTS --ask-sudo-pass --module-path ./ansible_modules --extra-vars "@config.json"
+fi
