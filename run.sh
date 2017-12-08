6c75f7723c24a97e645da017ac4aa967c3f785f2
kdaily <kdaily@users.noreply.github.com>
2017-12-08 13:07:42 -0800

    force install dnf

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # from https://github.com/JBKahn/provisioning-local/
 
-sudo dnf install python2-setuptools git python2-devel libxml2-devel libxslt-devel ansible python2-dnf
+sudo dnf install -y python2-setuptools git python2-devel libxml2-devel libxslt-devel ansible python2-dnf
 
 sudo easy_install pip
 
